a38528dd8b931e740c905f4ad4cbc250252c9e47
bodiseey <admin@bodishtyan.com>
2026-02-11 11:01:56 -0600

    remove dashboard and related auth logic

--- middleware.ts
+++ middleware.ts
@@ -1,53 +1,7 @@
-import { createServerClient, type CookieOptions } from '@supabase/ssr'
 import { NextResponse, type NextRequest } from 'next/server'
 
 export async function middleware(request: NextRequest) {
-    let response = NextResponse.next({
-        request: {
-            headers: request.headers,
-        },
-    })
-
-    const supabase = createServerClient(
-        process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-        {
-            cookies: {
-                getAll() {
-                    return request.cookies.getAll()
-                },
-                setAll(cookiesToSet) {
-                    cookiesToSet.forEach(({ name, value, options }) => request.cookies.set(name, value))
-                    response = NextResponse.next({
-                        request: {
-                            headers: request.headers,
-                        },
-                    })
-                    cookiesToSet.forEach(({ name, value, options }) =>
-                        response.cookies.set(name, value, options)
-                    )
-                },
-            },
-        }
-    )
-
-    const {
-        data: { user },
-    } = await supabase.auth.getUser()
-
-    const path = request.nextUrl.pathname;
-
-    // Protect Dashboard routes
-    if (path.startsWith('/dashboard') && !user) {
-        return NextResponse.redirect(new URL('/login', request.url))
-    }
-
-    // Redirect Authed User from Login/Register/Home to Dashboard
-    if ((path === '/login' || path === '/register' || path === '/') && user) {
-        return NextResponse.redirect(new URL('/dashboard', request.url))
-    }
-
-    return response
+    return NextResponse.next()
 }
 
 export const config = {
@@ -62,3 +16,4 @@ export const config = {
         '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
     ],
 }
+
